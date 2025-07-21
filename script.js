a8389500133e634462ed64599537c2e8aae6c50b
Jhon Alexander Suárez Sepúlveda <jhoals860912@gmail.com>
2025-07-21 10:44:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+
+  function enviarFormulario() {
+    alert("¡Gracias por tu mensaje! Me comunicaré contigo pronto.");
+    return false; // evita que recargue la página
+  }
+
+
+</script>
